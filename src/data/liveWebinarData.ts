183b426db6f9e2c71fa4abd21c0ec69d996c2b2e
Arvind Chaurasia <develop.akumar@gmail.com>
2025-09-13 17:22:34 +0530

    Tab position in desktop mode

--- src/data/liveWebinarData.ts
+++ src/data/liveWebinarData.ts
@@ -145,9 +145,102 @@ export const liveWebinarData: LiveWebinar[] = [
     followers: 200,
     type:"live",
     status : "Webinars based on cardiology"
-  }
+  },
+  {
+    id: 11,
+    imagePath: 'images/live/1.png',
+    credits: 2,
+    creditsExpiry: new Date('2025-09-12T04:30:00'),
+    title: 'Controversies in Biploar 1 Disorder (BP-1) implications for asthama from',
+    organisation: 'PGDI Indonesia',
+    orgLogoPath: 'images/small_logo/pgdi.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Asthama',
+    liveDate: new Date('2025-09-12T15:30:00'),
+    followers: 220,
+    type:"live",
+    status : "Live Webinars"
+  },
 
-  
+  {
+    id: 12,
+    imagePath: 'images/live/2.png',
+    credits: 1.5,
+    creditsExpiry: new Date('2025-09-12T15:30:00'),
+    title: 'Virus is like any other respiratory virus that causes common',
+    organisation: 'GSK',
+    orgLogoPath: 'images/small_logo/gsk.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Virus',
+    liveDate: new Date('2025-09-12T15:30:00'),
+    followers: 200,
+    type:"live",
+    status : "Live Webinars"
+  },
+
+  {
+    id: 13,
+    imagePath: 'images/live/3.png',
+    credits: 2,
+    creditsExpiry: new Date('2025-09-12T04:30:00'),
+    title: 'Virus is like any other respiratory virus that causes common',
+    organisation: 'Bayer',
+    orgLogoPath: 'images/small_logo/bayer.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Virus',
+    liveDate: new Date('2025-09-12T15:30:00'),
+    followers: 200,
+    type:"live",
+    status : "Live Webinars"
+  },
+
+   {
+    id: 24,
+    imagePath: 'images/upcoming/u1.png',
+    credits: 2,
+    creditsExpiry: new Date('2025-09-12T04:30:00'),
+    title: 'The Covid19 management of parox health caretine glucoma',
+    organisation: 'PGDI Indonesia',
+    orgLogoPath: 'images/small_logo/pgdi.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Asthama',
+    liveDate: new Date('2025-09-13T15:30:00'),
+    followers: 220,
+    type:"upcoming",
+    status : "Upcoming Webinars"
+  },
+
+  {
+    id: 25,
+    imagePath: 'images/upcoming/u2.png',
+    credits: 1.5,
+    creditsExpiry: new Date('2025-09-12T15:30:00'),
+    title: 'Perspective in the Covid19 Management of Parox Health syndrome',
+    organisation: 'GSK',
+    orgLogoPath: 'images/small_logo/gsk.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Virus',
+    liveDate: new Date('2025-09-14T15:30:00'),
+    followers: 200,
+    type:"upcoming",
+    status : "Upcoming Webinars"
+  },
+
+  {
+    id: 26,
+    imagePath: 'images/upcoming/u3.png',
+    credits: 2,
+    creditsExpiry: new Date('2025-09-12T04:30:00'),
+    title: 'The Covid19 management of parox health caretine glucome',
+    organisation: 'Bayer',
+    orgLogoPath: 'images/small_logo/bayer.png',
+    highlights: 'Manage chronic and complex diseases',
+    category: 'Virus',
+    liveDate: new Date('2025-09-15T15:30:00'),
+    followers: 200,
+    type:"upcoming",
+    status : "Upcoming Webinars"
+  },
 
 
 
